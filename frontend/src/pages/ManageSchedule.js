c7dbab3e8d8a788fc50f7cb525448a510ed09451
alchemist70 <herberzs70@gmail.com>
2025-07-15 14:27:37 +0530

    Configure for Render deployment

--- frontend/src/pages/ManageSchedule.js
+++ frontend/src/pages/ManageSchedule.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../styles/ManageSchedule.css";
-import Modal from "../components/Modal";
+import Modal from "../components/Modal.js";
 
 const ManageSchedule = () => {
   const navigate = useNavigate();
