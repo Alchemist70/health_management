aaec2b89cb33bb04814efa4cb87345de7a5dd11d
alchemist70 <herberzs70@gmail.com>
2025-07-16 12:26:05 +0530

    Some Error Corrections

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require("../controllers/userController");
 const { authenticateToken } = require("../middleware/auth");
 
 // Get user by ID
-router.get("/:id", authenticateToken, getUserById);
+router.get("/:id", authenticateToken, userController.getUserById);
 
 // Add route for updating user profile
 router.put("/profile", authenticateToken, userController.updateUserProfile);
